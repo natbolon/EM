2a552d68f375932c3c815d7c5931796db507fa8b
natbolon <natalie.bolonbrun@epfl.ch>
2018-03-06 15:00:52 +0100

    changes in new driver form

--- polls/forms.py
+++ polls/forms.py
@@ -7,6 +7,14 @@ class DriverForm(forms.ModelForm):
         attrs={'class': 'form-control col-md-6', 'placeholder': "Name"
                }
     ))
+    surname = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control col-md-6', 'placeholder': "Surname"}
+    ))
+    # email = forms.EmailField(widget=forms.EmailField())
+    # birthday = forms.DateField(widget=forms.DateField(input_formats=['%Y-%m-%d']))
+    # gender = forms.ChoiceField(widget=forms.ChoiceField())
+    # weight = forms.DecimalField(widget=forms.DecimalField)
+    # height = forms.DecimalField(widget=forms.DecimalField)
 
     class Meta:
         model = Driver
