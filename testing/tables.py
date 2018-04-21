6ca206f0f69738f414fadc97da0579a2bc3a09a9
natbolon <natalie.bolonbrun@epfl.ch>
2018-04-21 19:38:46 +0200

    Found how to call testing parameters into results table #Column(accessor = 'params.field_to_be_added')

--- testing/tables.py
+++ testing/tables.py
@@ -1,4 +1,4 @@
-from django_tables2 import tables
+from django_tables2 import tables, Column
 from django_tables2.export import TableExport
 
 from .models import Driver, Testing, Acceleration, Skid_Pad, AutoX, Endurance
@@ -19,15 +19,20 @@ class TestingTable(tables.Table):
 
 
 class AccelerationTable(tables.Table):
+    driver = Column(accessor='params.driver')
+
     class Meta:
         model = Acceleration
         template_name = 'django_tables2/bootstrap-responsive.html'
 
 
+
 class SkidPadTable(tables.Table):
+    driver = Column(accessor='params.driver')
     class Meta:
         model = Skid_Pad
         template_name = 'django_tables2/bootstrap-responsive.html'
+        sequence = ('id', 'l1_time', 'l2_time', 'r1_time', 'r2_time')
 
 
 class AutoXTable(tables.Table):
